0cdc19ad1b8ef4a268ecbbfa46ed15dd90777773
tinnamchoi <tinnam.choi@gmail.com>
2023-04-22 21:57:34 +0930

    ANZAC-2_Renaming

--- 2/A.cpp
+++ 2/A.cpp
@@ -2,26 +2,6 @@
 
 using namespace std;
 
-bool search(int cur, int mary, int marty, vector<int>& sushi, int mary_left, int mary_right, int marty_left, int marty_right){
-    // if the end
-    if (mary > mary_right || marty > marty_right) return false;
-    if (cur == sushi.size()){
-        if (mary >= mary_left && marty >= marty_left) return true;
-        return false;
-    }
-    // if even
-    bool ans = false;
-    if (sushi[cur]%2==0){
-        int t = sushi[cur]/2;
-        ans |= search(cur+1,mary+t,marty+t,sushi,mary_left,mary_right,marty_left,marty_right);
-    } else{
-        int t = sushi[cur]/2;
-        ans |= search(cur+1,mary+t,marty+t+1,sushi,mary_left,mary_right,marty_left,marty_right);
-        ans |= search(cur+1,mary+t+1,marty+t,sushi,mary_left,mary_right,marty_left,marty_right);
-    }
-    return ans;
-}
-
 int main() {
   int n;
   cin >> n;
@@ -29,19 +9,38 @@ int main() {
   int mary_l, mary_r, marty_l, marty_r;
   cin >> mary_l >> mary_r >> marty_l >> marty_r;
 
-  vector<int> sushi(n, 0);
+  int each = 0;
+  int over = 0;
+
   for (int i = 0; i < n; i++) {
     int temp;
     cin >> temp;
-    sushi[i] = temp;
+    if (temp == 1) {
+      cout << "No" << endl;
+      return 0;
+    }
+    each += temp / 2;
+    over += temp % 2;
   }
-  sort(sushi.begin(),sushi.end(),greater<int>());
 
-  // simulate
-  if (search(0,0,0,sushi,mary_l, mary_r, marty_l, marty_r)){
-    cout << "Yes" << endl;
-  } else{
+  if (each > mary_r || each > marty_r) {
     cout << "No" << endl;
+    return 0;
   }
-  return 0;
+
+  int budget = mary_r + marty_r - each * 2;
+
+  if (over > budget) {
+    cout << "No" << endl;
+    return 0;
+  }
+  
+  int missing = mary_l + marty_l - each * 2;
+  
+  if (missing > over) {
+    cout << "No" << endl;
+    return 0;
+  }
+
+  cout << "Yes" << endl;
 }
